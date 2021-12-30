59459a069b3686cc1578bb0596977494c7713ea1
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-30 13:54:56 +0300

    user reducer II commit

--- src/Components/redux/user-reducer.ts
+++ src/Components/redux/user-reducer.ts
@@ -1,6 +1,6 @@
 type UserType = {
     id: number
-    followed:boolean
+    followed: boolean
     fullName: string
     status: string
     location: { city: string, country: string }
@@ -8,21 +8,59 @@ type UserType = {
 
 type initStateType = Array<UserType>
 let initState: initStateType = [
-    {id: 1, followed:true, fullName: 'Vitalya', status: 'I am the best', location: {city: 'Brest', country: 'Belarus'}},
-    {id: 2, followed:false, fullName: 'Igor', status: 'I am the best of the best', location: {city: 'Brest', country: 'Belarus'}},
-    {id: 3, followed:false, fullName: 'Egor', status: 'I am the best of the best of the best', location: {city: 'Brest', country: 'Belarus'}},
-    {id: 4, followed:true, fullName: 'Genya', status: 'I am the best of the best of the best of the best', location: {city: 'Brest', country: 'Belarus'}},
-    {id: 5, followed:true, fullName: 'Romchik', status: 'I am the best of the best of the best of the best of the best', location: {city: 'Gomel', country: 'Belarus'}},
+    {
+        id: 1,
+        followed: true,
+        fullName: 'Vitalya',
+        status: 'I am the best',
+        location: {city: 'Brest', country: 'Belarus'}
+    },
+    {
+        id: 2,
+        followed: false,
+        fullName: 'Igor',
+        status: 'I am the best of the best',
+        location: {city: 'Brest', country: 'Belarus'}
+    },
+    {
+        id: 3,
+        followed: false,
+        fullName: 'Egor',
+        status: 'I am the best of the best of the best',
+        location: {city: 'Brest', country: 'Belarus'}
+    },
+    {
+        id: 4,
+        followed: true,
+        fullName: 'Genya',
+        status: 'I am the best of the best of the best of the best',
+        location: {city: 'Brest', country: 'Belarus'}
+    },
+    {
+        id: 5,
+        followed: true,
+        fullName: 'Romchik',
+        status: 'I am the best of the best of the best of the best of the best',
+        location: {city: 'Gomel', country: 'Belarus'}
+    },
 ]
 const UsersReducer = (state = initState, action: ActionsTypes): initStateType => {
     switch (action.type) {
-        case 'xxx': {
-            return state
+        case 'FOLLOW': {
+            return state.map(m => m.id === action.id ? {...m, followed: !m.followed} : m)
+        }
+        case 'UNFOLLOW': {
+            return state.map(m => m.id === action.id ? {...m, followed: !m.followed} : m)
         }
         default :
             return state
     }
 }
+type ActionsTypes = followACType | unFollowACType
 
-type ActionsTypes=
+type followACType = ReturnType<typeof followAC>
+export const followAC = (id: number) => ({type: 'FOLLOW', id})
+
+type unFollowACType = ReturnType<typeof unFollowAC>
+export const unFollowAC = (id: number) => ({type: 'UNFOLLOW', id})
 export default UsersReducer
