8f28cb6b2580ea0b699901c8d14f886e53f8aabb
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-03-04 17:23:05 +0300

    Paginator commit

--- src/Components/Common/Paginator/Paginator.tsx
+++ src/Components/Common/Paginator/Paginator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Paginator.module.css"
 
 
@@ -7,21 +7,35 @@ type PaginatorPropsType = {
     pageSize: number
     currentPage: number
     onPageChanged: (pageNumber: number) => void
+    portionSize?: number
 }
-export const Paginator = ({totalCount, pageSize, onPageChanged, currentPage}: PaginatorPropsType) => {
+export const Paginator = ({totalCount, pageSize, onPageChanged, currentPage, portionSize = 10}: PaginatorPropsType) => {
     let pagesCount = Math.ceil(totalCount / pageSize)
     let pages = []
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
 
-    return (
-        <div className={style.pages}>{pages.map((p, i) => {
-            return <span key={i} onClick={() => {
-                onPageChanged(p)
-            }} className={currentPage === p ? style.selected : ''}>-{p}-</span>
-        })}
-        </div>
-    )
+    let portionCount = Math.ceil(pagesCount / portionSize)
+    const [portionNumber, setPortionNumber] = useState<number>(1)
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+    let rightPortionPageNumber = portionNumber * portionSize
+
+    return <div className={style.pages}>
+        {portionNumber > 1 && <button onClick={() => {
+            setPortionNumber(portionNumber - 1)
+        }}>prev</button>}
+        {pages.filter(f => f >= leftPortionPageNumber && f <= rightPortionPageNumber)
+            .map((p, i) => {
+                return <span key={i}
+                             onClick={() => {
+                                 onPageChanged(p)
+                             }}
+                             className={currentPage === p ? style.selected : ''}>-{p}-</span>
+            })}
+        {portionCount > portionNumber && <button onClick={() => {
+            setPortionNumber(portionNumber + 1)
+        }}>next</button>}
+    </div>
 }
 
--- src/Components/Profile/ProfileStatus.test.tsx
+++ src/Components/Profile/ProfileStatus.test.tsx
@@ -12,7 +12,7 @@ describe("ProfileStatus component", () => {
         expect(instance.state.status).toBe('blabla')
     });
 
-    test("After creation span with status should be displayed", () => {
+    test("After creation <span> with status should be displayed", () => {
         const component = create(<ProfileStatus status='blabla' updateStatus={() => {
         }}/>);
         const root = component.root;
@@ -21,16 +21,45 @@ describe("ProfileStatus component", () => {
         expect(span).not.toBeNull()
     });
 
-    test("After creation span with status should be correct", () => {
+    test("After creation <input>  shouldn`t be displayed", () => {
+        const component = create(<ProfileStatus status='blabla' updateStatus={() => {
+        }}/>);
+        const root = component.root;
+        expect(() => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const input = root.findByType("input");
+        }).toThrow()
+    });
+
+    test("After creation <span> with status should be correct", () => {
         const component = create(<ProfileStatus status='blabla' updateStatus={() => {
         }}/>);
         const root = component.root;
         // eslint-disable-next-line testing-library/await-async-query
         const span = root.findByType("span");
         expect(span.children[1]).toBe('blabla')
+    });
 
+    test(" <input> should be displayed in editMode instead of <span>", () => {
+        const component = create(<ProfileStatus status='blabla' updateStatus={() => {
+        }}/>);
+        const root = component.root;
+        // eslint-disable-next-line testing-library/await-async-query
+        const span = root.findByType("span");
+        span.props.onDoubleClick()
+        // eslint-disable-next-line testing-library/await-async-query
+        const input = root.findByType("input");
 
-        // const testInstance = component.root;
-        // expect(testInstance.findByType("span")).toBe(1);
+        expect(input.props.value).toBe('blabla')
+    });
+
+    test(" Callback should be called", () => {
+        const mockCallback=jest.fn()
+        const component = create(<ProfileStatus status='blabla' updateStatus={mockCallback}/>);
+        const instance = component.getInstance();
+        // @ts-ignore
+        instance.deactivateEditMode()
+
+        expect(mockCallback.mock.calls.length).toBe(1)
     });
 });
