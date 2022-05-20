72fa74d12f364784853b51a893bc1d3f6fe4cf41
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-05-20 13:16:57 +0200

    second test passed  commit

--- src/Components/redux/user-reducer.test.ts
+++ src/Components/redux/user-reducer.test.ts
@@ -1,6 +1,6 @@
 import UsersReducer, {initialStateType, usersActions} from "./user-reducer";
 
-test('', () => {
+test('user followSuccess test', () => {
     const state: initialStateType = {
         users: [
             {
@@ -38,4 +38,44 @@ test('', () => {
     expect(newState.users[1].followed).toBeTruthy()
     expect(newState.users[1].id).toBe(2)
     expect(newState.users[1].name).toBe('Nastya')
+})
+
+test('user unFollowSuccess test', () => {
+    const state: initialStateType = {
+        users: [
+            {
+                id: 1, name: 'Vlad', followed: false,
+                photos: {small: null, large: null}, status: 'status1',
+                location: {city: 'Warsaw', country: 'Poland'}
+            },
+            {
+                id: 2, name: 'Nastya', followed: false,
+                photos: {small: null, large: null}, status: 'status2',
+                location: {city: 'Warsaw', country: 'Poland'}
+            },
+            {
+                id: 3, name: 'Mama', followed: true,
+                photos: {small: null, large: null}, status: 'status3',
+                location: {city: 'Brest', country: 'Belarus'}
+            },
+            {
+                id: 4, name: 'Papa', followed: false,
+                photos: {small: null, large: null}, status: 'status4',
+                location: {city: 'Brest', country: 'Belarus'}
+            }
+        ],
+        pageSize: 90,
+        totalCount: 0,
+        currentPage: 2,
+        isFetching: true,
+        followingInProgress: false,
+        followingId: []
+    }
+
+    const newState = UsersReducer(state, usersActions.unFollowSuccess(4))
+
+    expect(newState.users[3].followed).toBeFalsy()
+    expect(newState.users[2].followed).toBeTruthy()
+    expect(newState.users[3].id).toBe(4)
+    expect(newState.users[3].name).toBe('Papa')
 })
