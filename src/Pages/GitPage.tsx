ac9fba6939eb2ced9ce70305d3506824e38ef5e8
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-06-02 16:22:33 +0200

    search with query realised commit

--- src/Pages/GitPage.tsx
+++ src/Pages/GitPage.tsx
@@ -3,41 +3,51 @@ import axios from "axios";
 
 type SearchUserType = {
     login: string
+    id: number
 }
 type SearchResult = {
     items: SearchUserType[]
 }
 const GitPage = () => {
-    const [selectedU, setSelectedU] = useState<string | null>(null)
-
+    const [selectedU, setSelectedU] = useState<SearchUserType | null>(null)
+    const [u, setU] = useState<SearchUserType[]>([] as SearchUserType[])
+    const [tempSearch, setTempSearch] = useState<string>('')
+    const search = () => {
+        axios.get<SearchResult>(`https://api.github.com/search/users?q=${tempSearch}`)
+            .then((res) => {
+                setU(res.data.items)
+            })
+    }
     useEffect(() => {
         if (selectedU) {
-            document.title = selectedU
+            document.title = selectedU.login
         }
     }, [selectedU])
 
     useEffect(() => {
         axios.get<SearchResult>('https://api.github.com/search/users?q=it-kamasutra')
-            .then((res)=>{
-                console.log(res)
+            .then((res) => {
+                setU(res.data.items)
             })
     }, [])
     return (
         <div style={{display: 'flex'}}>
             <div>
                 <div>
-                    <input type="text" placeholder={'saerch'}/>
-                    <button>find</button>
+                    <input type="text" placeholder={'saerch'}
+                           value={tempSearch}
+                           onChange={(e) => setTempSearch(e.currentTarget.value)}/>
+                    <button onClick={search}>find</button>
                 </div>
                 <ul>
-                    {['user I', 'user II', 'user III', 'user IV', 'user V', 'user VI', 'user VI I'].map((m, i) => {
-                            return <li key={i}
+                    {u.map((m) => {
+                            return <li key={m.id}
                                        onClick={() => {
                                            setSelectedU(m)
 
                                        }}
                                        style={selectedU === m ? {color: 'red'} : {}}>
-                                {m}</li>
+                                {m.login}</li>
                         }
                     )}
                 </ul>
