57d69deec23c87bf6c241d7d013f27faf923e8ad
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-06-02 15:14:10 +0200

    change color commit

--- src/Pages/GitPage.tsx
+++ src/Pages/GitPage.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const GitPage = () => {
+    const [selectedU, setSelectedU] = useState<string | null>(null)
     return (
-        <div>
+        <div style={{display: 'flex'}}>
             <div>
                 <div>
                     <input type="text" placeholder={'saerch'}/>
                     <button>find</button>
                 </div>
                 <ul>
-                    {['dcwedfc', 'cae`adfc'].map((m, i) => <li key={i}>{m}</li>)}
+                    {['dcwedfc', 'cae`adfc'].map((m, i) => <li key={i} onClick={() => {
+                        document.title = m
+                        setSelectedU(m)
+                    }}
+                                                               style={selectedU === m ? {color: 'red'} : {}}>{m}</li>)}
                 </ul>
             </div>
             <div>
