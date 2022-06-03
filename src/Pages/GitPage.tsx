0661e2bd6b5ca02d6030d5bb1597241f9f4ffdf1
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-06-03 12:39:02 +0200

    usersDetails in plashka commit

--- src/Pages/GitPage.tsx
+++ src/Pages/GitPage.tsx
@@ -8,8 +8,15 @@ type SearchUserType = {
 type SearchResult = {
     items: SearchUserType[]
 }
+type UserType = {
+    login: string
+    id: number
+    avatar_url: string
+    folowers: number
+}
 const GitPage = () => {
     const [selectedU, setSelectedU] = useState<SearchUserType | null>(null)
+    const [uDetails, setUDetails] = useState<UserType | null>(null)
     const [u, setU] = useState<SearchUserType[]>([] as SearchUserType[])
     const [tempSearch, setTempSearch] = useState<string>('it-kamasutra')
     const [searchTerm, setSearchTerm] = useState<string>('it-kamasutra')
@@ -26,11 +33,21 @@ const GitPage = () => {
     }, [selectedU])
 
     useEffect(() => {
-        axios.get<SearchResult>(`https://api.github.com/search/users?q=${searchTerm}`)
+        axios
+            .get<SearchResult>(`https://api.github.com/search/users?q=${searchTerm}`)
             .then((res) => {
                 setU(res.data.items)
             })
     }, [searchTerm])
+
+    useEffect(() => {
+        if (!!selectedU) {
+            axios.get<UserType>(`https://api.github.com/users/${selectedU.login}`)
+                .then((res) => {
+                    setUDetails(res.data)
+                })
+        }
+    }, [selectedU])
     return (
         <div style={{display: 'flex'}}>
             <div>
@@ -55,8 +72,8 @@ const GitPage = () => {
                 </ul>
             </div>
             <div>
-                <h2>User name</h2>
-                <div>details</div>
+                <h2>login: {uDetails?.login}</h2>
+                <img src={uDetails?.avatar_url} alt={'cdjshg'}/>
             </div>
         </div>
     );
